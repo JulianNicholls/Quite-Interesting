20661fb6bd41f5fd5913e7c5c984c19c37957bdd
Julian Nicholls <juliannicholls29@gmail.com>
2020-02-14 16:35:53 +0000

    Initial display of episodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import { Store, FETCH_DATA } from './store';
+
 import './App.css';
 
+interface IImages {
+  medium: string;
+  original: string;
+}
+
+interface IEpisode {
+  id: number;
+  name: string;
+  season: number;
+  number: number;
+  image: IImages;
+  summary: string;
+}
+
+const URL: string =
+  'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+
 const App = () => {
+  const { state, dispatch } = useContext(Store);
+
+  useEffect(() => {
+    state.episodes.length === 0 && fetchData();
+  });
+
+  const fetchData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    dispatch({ type: FETCH_DATA, payload: data._embedded.episodes });
+  };
+
   return (
     <>
       <h1>Rick and Morty</h1>
       <p>Pick your favourite episode.</p>
+      <section>
+        {state.episodes.map((e: IEpisode) => (
+          <article key={e.id}>
+            <img src={e.image.medium} alt={e.name} />
+            <div>{e.name}</div>
+            <section>
+              Episode S{e.season}:E{e.number}
+            </section>
+          </article>
+        ))}
+      </section>
     </>
   );
 };
