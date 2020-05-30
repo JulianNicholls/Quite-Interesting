1b6ef68e83672ff0477d167849847639b2e1c771
Julian Nicholls <juliannicholls29@gmail.com>
2020-05-30 17:35:57 +0100

    Re-style.

--- src/components/EpisodeCard.tsx
+++ src/components/EpisodeCard.tsx
@@ -6,29 +6,33 @@ interface ECProps {
   toggle: (episode: Episode) => void;
 }
 
-const EpisodeCard = ({ episode, favourite, toggle }: ECProps): JSX.Element => (
-  <article className="episode-box">
-    <img
-      src={
-        episode.image !== null && episode.image.medium
-          ? episode.image.medium
-          : 'https://via.placeholder.com/250x140&text=No%20Pickles'
-      }
-      alt={episode.name}
-    />
+const EpisodeCard = ({ episode, favourite, toggle }: ECProps): JSX.Element => {
+  const { image } = episode;
 
-    <div>{episode.name}</div>
-    <section>
-      Episode S{episode.season}, E{episode.number}
-      <button
-        className="favourite-button"
-        type="button"
-        onClick={() => toggle(episode)}
-      >
-        {favourite ? 'Remove' : 'Favourite'}
-      </button>
-    </section>
-  </article>
-);
+  return (
+    <article className="episode-box">
+      <img
+        src={
+          image !== null && image.medium
+            ? image.medium
+            : 'https://via.placeholder.com/250x140/eeeeee/333333&text=No+Alans'
+        }
+        alt={episode.name}
+      />
+
+      <div>{episode.name}</div>
+      <section>
+        Season {episode.season}, Ep. {episode.number}
+        <button
+          className="favourite-button"
+          type="button"
+          onClick={() => toggle(episode)}
+        >
+          {favourite ? 'Remove' : 'Favourite'}
+        </button>
+      </section>
+    </article>
+  );
+};
 
 export default EpisodeCard;
